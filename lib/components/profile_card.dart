3575e49ca665a9114c34fa1a29aa160ce54435ea
tarimansari1 <tarimansari1@gmail.com>
2025-06-26 10:05:43 +0000

    Used Stack tool inside profile_card.dart to add a blue check over Profile picture

--- lib/components/profile_card.dart
+++ lib/components/profile_card.dart
@@ -9,12 +9,20 @@ class ProfileCard extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        CircleAvatar(
-          radius: 60,
-          backgroundImage: NetworkImage(
-            'https://media.licdn.com/dms/image/v2/D5603AQGY6Zxjl5yELw/profile-displayphoto-shrink_200_200/B56Zab35_VHAAY-/0/1746371846881?e=2147483647&v=beta&t=4Ys1Dz8i54nOwSiY3TsLGrm4X9H6V_90fiL3okSge4c',
-          ),
-        ),
+       Stack(
+                children: [
+                  CircleAvatar(
+                radius: 60,
+                backgroundImage: NetworkImage(
+                  'https://media.licdn.com/dms/image/v2/D5603AQGY6Zxjl5yELw/profile-displayphoto-shrink_200_200/B56Zab35_VHAAY-/0/1746371846881?e=2147483647&v=beta&t=4Ys1Dz8i54nOwSiY3TsLGrm4X9H6V_90fiL3okSge4c',
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                right: 4,
+                child: Icon(Icons.check_circle, color: Colors.blue,))
+                ],
+              ),
         SizedBox(height: 10),
         Text(
           "Hello, I'm Tarim!",
